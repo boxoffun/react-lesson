653339a36ea4b564974a9b9fc2b0d30c15e70609
Aigul <boxoffun96@gmail.com>
2019-11-11 21:57:07 +0200

    Updates

---
+++ precache-manifest.3f67cb2c1fa06939e4d66a91fe646087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c5509db21152e243d6d3da14f7ec5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5305255d5b0cdb7fc8d",
+    "url": "/static/css/main.a9d0e2b9.chunk.css"
+  },
+  {
+    "revision": "ff7b00600d233d974e30",
+    "url": "/static/js/2.d20f16af.chunk.js"
+  },
+  {
+    "revision": "b5305255d5b0cdb7fc8d",
+    "url": "/static/js/main.427b188b.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
